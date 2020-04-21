1a3b45e84e5df8dc39f8ba7f16ad6de1a31cb7f3
asifiwem <asifiwemanzi@gmail.com>
2020-04-21 16:53:19 +0200

    Add procfile for heroku

--- src/app.js
+++ src/app.js
@@ -6,11 +6,11 @@ import {} from 'dotenv/config';
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 const app = express();
-const port = process.env.port;
+const port = process.env.port || 3000;
 
 app.use(express.json());
 
 app.use(routes);
 
 app.listen(port);
-console.log('App running on Localhost:', port);
+console.log('App running on Localhost:' + port);
